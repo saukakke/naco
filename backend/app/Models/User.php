9ee16575c5279470d7bc6ac01a98942a8054825e
Yasin Muhammed Tukur <saukakke@gmail.com>
2026-08-18 02:48:22 +0100

    Enable Sanctum tokens on NACO users

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
+    use HasApiTokens, Notifiable;
     protected $fillable = ['name', 'email', 'password', 'role', 'cadet_id'];
     protected $hidden = ['password', 'remember_token'];
     protected function casts(): array { return ['email_verified_at' => 'datetime', 'password' => 'hashed']; }
