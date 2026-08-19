0de3a06f83899f9cbe29d7e8ca782d988789ba53
Yasin Muhammed Tukur <saukakke@gmail.com>
2026-08-19 06:31:05 +0100

    Make PostgreSQL the Laravel default database connection

--- backend/config/database.php
+++ backend/config/database.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Str;
 
 return [
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
     'connections' => [
         'sqlite' => [
             'driver' => 'sqlite',
@@ -18,11 +18,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST','127.0.0.1'),
-            'port' => env('DB_PORT','5432'),
-            'database' => env('DB_DATABASE','naco'),
-            'username' => env('DB_USERNAME','postgres'),
-            'password' => env('DB_PASSWORD',''),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'naco'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -30,6 +30,23 @@ return [
             'sslmode' => 'prefer',
         ],
     ],
-    'migrations' => ['table'=>'migrations','update_date_on_publish'=>true],
-    'redis' => ['client'=>env('REDIS_CLIENT','phpredis'),'options'=>['cluster'=>env('REDIS_CLUSTER','redis'),'prefix'=>env('REDIS_PREFIX',Str::slug((string)env('APP_NAME','laravel')).'-database-')],'default'=>['url'=>env('REDIS_URL'),'host'=>env('REDIS_HOST','127.0.0.1'),'username'=>env('REDIS_USERNAME'),'password'=>env('REDIS_PASSWORD'),'port'=>env('REDIS_PORT',6379),'database'=>env('REDIS_DB',0)]],
+    'migrations' => [
+        'table' => 'migrations',
+        'update_date_on_publish' => true,
+    ],
+    'redis' => [
+        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
+        ],
+        'default' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
+        ],
+    ],
 ];
